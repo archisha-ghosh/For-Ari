39f43d52e4a213d6350c9fa7ea167652891d718c
archisha-ghosh <72865571+archisha-ghosh@users.noreply.github.com>
2021-05-16 02:57:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Ari Parker"&&b=="#Ethanari4Life")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
